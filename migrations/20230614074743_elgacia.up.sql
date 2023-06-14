c0a8013a0bea022be073f7339e427cf16ef93ca5
Orcthanc <orcthanclp@gmail.com>
2023-06-14 10:16:43 +0200

    Fixed migration

--- migrations/20230614074743_elgacia.up.sql
+++ migrations/20230614074743_elgacia.up.sql
@@ -4,3 +4,9 @@ INSERT INTO raids (name, difficulty, required_item_level, three_weekly) VALUES
     ("Kayangel 3/4", "Normal", 1540, 0),
     ("Kayangel 1/2", "Hard", 1580, 1),
     ("Kayangel 3/4", "Hard", 1580, 0);
+
+INSERT INTO raid_prerequisites (raid, requires)
+    (SELECT r.id, rr.id
+    FROM raids r
+    JOIN raids rr
+    WHERE r.name = "Kayangel 3/4" AND rr.name = "Kayangel 1/2");
